153b1bae7789d99ebdb1c53ef03f203ea48844bf
Ludwig ZIETSMAN <lzietsma@e4r2p4.wethinkcode.co.za>
2018-03-05 13:38:36 +0200

    Added struct

--- reading/reader.h
+++ reading/reader.h
@@ -6,7 +6,7 @@
 /*   By: wgourley <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/05 12:39:53 by wgourley          #+#    #+#             */
-/*   Updated: 2018/03/05 12:49:15 by wgourley         ###   ########.fr       */
+/*   Updated: 2018/03/05 13:38:02 by lzietsma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,14 @@
 # define READER_H
 # define BUFFER_SIZE 1024
 
-char	**read_map(char *file_name);
+char			**read_map(char *file_name);
+
+typedef	struct	s_map_param
+{
+	int			lines;
+	char		empty;
+	char		obstacle;
+	char		full;
+}				t_map_param;
 
 #endif
