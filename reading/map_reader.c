229e47a5aca4dd1721ef716aa0cb4754075bc5f4
wgourley <wgourley@student.wethinkcode.co.za>
2018-03-05 13:46:00 +0200

    reading a line

--- reading/map_reader.c
+++ reading/map_reader.c
@@ -6,7 +6,7 @@
 /*   By: wgourley <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/05 12:38:44 by wgourley          #+#    #+#             */
-/*   Updated: 2018/03/05 13:21:34 by wgourley         ###   ########.fr       */
+/*   Updated: 2018/03/05 13:44:50 by wgourley         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,8 @@
 #include "../errors/std_error.h"
 #include <fcntl.h>
 
-
+char		**g_map;
+t_map_param	*g_map_paramaters = 0;
 
 char 	**read_map(char *file_name)
 {
@@ -29,8 +30,47 @@ char 	**read_map(char *file_name)
 	buffer = (char *)malloc(sizeof(char *) * BUFFER_SIZE);
 	while ((read_count = read(file, buffer, BUFFER_SIZE)))
 	{
-		write(1, buffer, BUFFER_SIZE);
+		handle_lines(buffer, read_count);
 		free(buffer);
 		buffer = (char *)malloc(sizeof(char *) * BUFFER_SIZE);
+	}
+}
+
+char *get_line(char *data, int length)
+{
+	char	*ret;
+	int		index;
+
+	index = 0;
+	ret = malloc(sizeof(ret) * (length));
+	while (index < length)
+	{
+		ret[index] = data[index];
+		index++;
+	}
+	return (ret);
+}
+
+void	handle_lines(char *data, int read_count)
+{
+	int		index;
+	int		last;
+	char	*line;
+
+	index = 0;
+	last = 0;
+	while (index < read_count)
+	{
+		if (data[index] == '\n')
+		{
+			line = get_line(data + last, index - last);
+			printf("%s", line);
+			exit(0);
+			if(!g_map_paramaters)
+			{
+				// make params here
+			}
+		}
+		index++;
 	}
 }
--- reading/reader.h
+++ reading/reader.h
@@ -6,7 +6,7 @@
 /*   By: wgourley <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/05 12:39:53 by wgourley          #+#    #+#             */
-/*   Updated: 2018/03/05 13:38:02 by lzietsma         ###   ########.fr       */
+/*   Updated: 2018/03/05 13:45:45 by wgourley         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 # define BUFFER_SIZE 1024
 
 char			**read_map(char *file_name);
+void			handle_lines(char *data, int read_count);
 
 typedef	struct	s_map_param
 {
