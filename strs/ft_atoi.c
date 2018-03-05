ca16bd8a3ded5cdf92b09787b73c4f70c8d1c482
Ludwig ZIETSMAN <lzietsma@e4r2p4.wethinkcode.co.za>
2018-03-05 14:07:47 +0200

    Changed atoi to accept the number of elements to read from [0]

--- strs/ft_atoi.c
+++ strs/ft_atoi.c
@@ -6,17 +6,17 @@
 /*   By: lzietsma <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/05 12:42:11 by lzietsma          #+#    #+#             */
-/*   Updated: 2018/03/05 13:42:25 by lzietsma         ###   ########.fr       */
+/*   Updated: 2018/03/05 14:06:34 by lzietsma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_atoi(char *c_array)
+int	ft_atoi(char *c_array, int elem_to_read)
 {
 	int i;
 	int value;
 
 	i = 0;
-	while (c_array[i])
+	while (i < elem_to_read)
 	{
 		value = 10 * value + c_array[i] - '0';
 		i++;
