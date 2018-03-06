302594dc352726fbb6c57f511b552a7ad54ad136
wgourley <wgourley@student.wethinkcode.co.za>
2018-03-06 13:56:22 +0200

    dog gamit

--- new_code/square_init.c
+++ new_code/square_init.c
@@ -6,19 +6,19 @@
 /*   By: lzietsma <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/06 11:12:14 by lzietsma          #+#    #+#             */
-/*   Updated: 2018/03/06 13:52:39 by lzietsma         ###   ########.fr       */
-/*   Updated: 2018/03/06 13:35:51 by wgourley         ###   ########.fr       */
+/*   Updated: 2018/03/06 13:55:59 by wgourley         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "square_manip.h"
 #include "../reading/reader.h"
+#include <stdlib.h>
 
-t_square_manip	*square_init(t_map_param *params)
+t_square_manip	*square_init(char c_array, t_map_param *params)
 {
-	square_manip	*ret;
-
 	t_square_manip	*ret;
+
+	ret = malloc(sizeof(ret));
 	if (params->map_length < params->lines)
 		ret->dimension = params->map_length;
 	else
