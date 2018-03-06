4a30acb295751d9b6737f2f3661d240a01034b5f
Ludwig ZIETSMAN <lzietsma@e4r2p4.wethinkcode.co.za>
2018-03-06 13:32:08 +0200

    Ludwig

--- new_code/square_init.c
+++ new_code/square_init.c
@@ -6,7 +6,7 @@
 /*   By: lzietsma <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/06 11:12:14 by lzietsma          #+#    #+#             */
-/*   Updated: 2018/03/06 13:29:30 by lzietsma         ###   ########.fr       */
+/*   Updated: 2018/03/06 13:31:58 by lzietsma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 t_square_manip	*square_init(char c_array, t_map_param *params)
 {
 	square_manip	*ret;
+
 	if (params->map_length < params->lines)
 		ret->dimension = params->map_length;
 	else
