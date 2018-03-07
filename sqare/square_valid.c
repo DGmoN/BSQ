4a9e2692a1bafcc896bac82f1ae650fe80051c2b
Ludwig ZIETSMAN <lzietsma@e4r2p4.wethinkcode.co.za>
2018-03-07 11:58:37 +0200

    Ludwig

--- sqare/square_valid.c
+++ sqare/square_valid.c
@@ -6,7 +6,7 @@
 /*   By: lzietsma <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/06 13:32:28 by lzietsma          #+#    #+#             */
-/*   Updated: 2018/03/07 11:22:03 by wgourley         ###   ########.fr       */
+/*   Updated: 2018/03/07 11:54:15 by lzietsma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,10 @@ int		square_valid(t_square_manip *blk, t_map_param *param, char **map)
 	int		c_line;
 	int		c_char;
 
-	c_line = 0;
+	c_line = blk->y;
 	while (c_line < blk->dimension)
 	{
-		c_char = 0;
+		c_char = blk->x;
 		while (c_char < blk->dimension)
 		{
 			printf("Test: (%i, %i) -> %i -> %i\n", c_line, c_char, map[c_line][c_char], param->obstacle);
