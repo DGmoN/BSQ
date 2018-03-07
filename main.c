96aa232f021368ce51650e4628a45244dcc012f2
wgourley <wgourley@student.wethinkcode.co.za>
2018-03-07 11:39:43 +0200

    duckit

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: wgourley <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/05 12:29:48 by wgourley          #+#    #+#             */
-/*   Updated: 2018/03/06 16:49:59 by wgourley         ###   ########.fr       */
+/*   Updated: 2018/03/07 11:34:01 by wgourley         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,24 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include "sqare/square_manip.h"
+
+void	fillin(t_map_param *par, t_square_manip *block, char** map)
+{
+	int	x;
+	int y;
+
+	y = 0;
+	while(y < block->dimension)
+	{
+		x = 0;
+		while (x < block->dimension)
+		{
+			map[block->y + y][block->x + x] = par->full;
+			x++;
+		}
+		y++;
+	}
+}
 
 int main(int ac, char **av)
 {
@@ -30,7 +48,9 @@ int main(int ac, char **av)
 
 	map = read_map(file, &params);
 	block = square_init(params);
+	move_square(params, block, map);
 	int x = 0;
+	fillin(params, block, map);
 	while (x < params->lines)
 	{
 		printf("%s\n", map[x]);
--- sqare/move_square.c
+++ sqare/move_square.c
@@ -3,19 +3,22 @@
 
 void	move_square(t_map_param *param, t_square_manip *block, char **map)
 {
-	int *fit_size;
-	
-	fit_size = &block->dimension;
-	while (*fit_size > 0 && !square_valid(block, param, map))
+	while (block->dimension > 0)
 	{
+		if (square_valid(block, param, map))
+			break ;
 		if (can_go_left(param, block))
 			move_left(block);
-		else if(can_go_down(param, block))
-			move_down(block);
 		else
-			reset(block);
-		
-	}
-	if (*fit_size == 0)
+		{
+			if(can_go_down(param, block))
+				move_down(block);
+			else
+				reset(block);
+		}
+		printf("testing: (%i, %i) %i\n", block->x, block->y, block->dimension);
+		}
+	if (block->dimension == 0)
 		size_error();
+	printf("Block fit: (%i, %i) %i\n", block->x, block->y, block->dimension);
 }
